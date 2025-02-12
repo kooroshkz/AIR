e89a5f285de7bd973150edcf930ed0aee70ebe4d
Johannes Leppäkorpi <user3610@protonmail.com>
2025-02-12 15:49:25 +0100

    cicd amend update test

--- main.py
+++ main.py
@@ -4,26 +4,26 @@ from src.napari_image_filtering_interface import napari_experimental_provide_doc
 
 
 def main():
-    """
-    Main entry point to launch Napari
-    """
-    # Initialize .env
-    load_dotenv()
-    # Create a new Napari viewer
-    viewer = napari.Viewer()
+                                """
+                                Main entry point to launch Napari
+                                """
+                                # Initialize .env
+                                load_dotenv()
+                                # Create a new Napari viewer
+                                viewer = napari.Viewer()
 
-    # Get the dock widget factory
-    dock_widget_factory = napari_experimental_provide_dock_widget()
+                                # Get the dock widget factory
+                                dock_widget_factory = napari_experimental_provide_dock_widget()
 
-    # Add the dock widget to the viewer
-    viewer.window.add_dock_widget(
-        dock_widget_factory(viewer),
-        area='right',
-        name='Image Filters'
-    )
+                                # Add the dock widget to the viewer
+                                viewer.window.add_dock_widget(
+                                    dock_widget_factory(viewer),
+                                    area='right',
+                                    name='Image Filters'
+                                )
 
-    # Start the Napari event loop
-    napari.run()
+                                # Start the Napari event loop
+                                napari.run()
 
 
 if __name__ == "__main__":
